6d7cf551a95dba88f29fb29767faf9f31df097aa
sandeepshetty <sandeep.shetty@gmail.com>
2011-04-18 03:49:03 +0530

    Added a link to home in the header of all POST result pages.

--- templates/error.html.php
+++ templates/error.html.php
@@ -2,6 +2,7 @@
 
 	<div data-role="header" data-backbtn="true">
 		<h1>Error</h1>
+		<a href="<?php echo absolute_uri('/') ?>" data-icon="home" class="ui-btn-right" data-iconpos="notext">Home</a>
 	</div>
 
 	<div data-role="content">
